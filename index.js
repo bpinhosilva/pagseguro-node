1714e22c4d300bd57cdbb620060c6461a3142176
Bruno Pinho <bpinhosilva@gmail.com>
2016-02-05 01:37:12 -0200

    Added buyer and shipment objects. Created main structure to send requests to PagSeguro API. Added a few more parameters to main class.

--- index.js
+++ index.js
@@ -5,9 +5,17 @@
 
 "use strict";
 
+var request = require('request');
+var parseString = require('xml2js').parseString;
+
 var Item = require('./lib/item');
+var Buyer = require('./lib/buyer');
+var Shipment = require('./lib/shipment');
+var URL_API_PAY = 'https://ws.pagseguro.uol.com.br/v2/checkout';
 
 var PagSeguro = function (email, token, charset = 'UTF-8') {
+
+  var self = this;
 
   if (arguments.length === 3) {
     self.charset = charset.toString().toUpperCase();
@@ -17,8 +25,8 @@ var PagSeguro = function (email, token, charset = 'UTF-8') {
       return false;
     }
     
-    self.email    = email;
-    self.token    = token;
+    self.email = email;
+    self.token = token;
   }
   else if (arguments.length == 2) {
     self.token = token;
@@ -29,9 +37,19 @@ var PagSeguro = function (email, token, charset = 'UTF-8') {
      return false;
   }
 
-  var self = this;
-
   self.items = [];
+  self.buyer = {};
+  self.shipment = {};
+  self.redirectURL = '';
+  self.notificationURL = '';
+  self.maxUses = 1000;
+  self.maxAge = 1000;
+
+  var mountQueryString = function () {
+    var qs = {};
+
+    return qs;
+  };
 
   return {
     getItemsLength : function () {
@@ -45,6 +63,50 @@ var PagSeguro = function (email, token, charset = 'UTF-8') {
         if (self.items[i].getId() === id) return self.items[i];
       }
       return null;
+    },
+    addBuyer : function (buyer) {
+      self.buyer.push(new Buyer(buyer));
+    },
+    addShipment : function (spm) {
+      self.shipment.push(new Shipment(spm));
+    },
+    processOrder : function (callback) {
+
+      if (!callback || typeof callback != "function") {
+        throw "Error: you must provide a callback for processOrder";
+        return null;
+      }
+
+      var options = {
+        url: URL_API_PAY,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=' + self.charset;
+        }
+      };
+
+      options.qs = mountQueryString();
+
+      request.post(options, function (error, response, body) {
+
+        // parse xml returned from pagSeguro API
+        parseString(body, function (err, result) {
+
+          // error reading xml
+          if (err) {
+            throw "Error: reading xml file";
+            return null;
+          }
+
+          // got good response
+          if (!error && response.statusCode == 200) {            
+            callback(null, response, result.checkout);
+          }
+          else {  // got error
+            callback(result.errors.error, response, null); 
+          }
+        });
+      });
+
     }
   };
 
